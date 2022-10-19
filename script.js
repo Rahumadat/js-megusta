fcfbd887c58cb32b8115c8c857b3ab5421804ffb
Rodrigo <rahumadat@gmail.com>
2022-10-19 01:19:02 -0300

    paso 2 de sumar likes realizado

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function sumLike() {
-        let contalike = document.getElementById("numlikes").innerText;
-    contalike ++;
-    document.getElementById("numlikes").innerText = contalike;
+function sumLike1() {
+    let contalike1 = document.getElementById("numlikes1").innerText;
+    contalike1 ++;
+    document.getElementById("numlikes1").innerText = contalike1;
 }
-console.log(contalike);
-// }
+function sumLike2() {
+    let contalike2 = document.getElementById("numlikes2").innerText;
+    contalike2 ++;
+    document.getElementById("numlikes2").innerText = contalike2;
+}
+function sumLike3() {
+    let contalike3 = document.getElementById("numlikes3").innerText;
+    contalike3 ++;
+    document.getElementById("numlikes3").innerText = contalike3;
+}
